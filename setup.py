ac157dda7c00dacd845116c2f2d8f5853945d82d
jarbasai <jarbasai@mailfence.com>
2022-03-24 01:20:43 +0000

    :tada:

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 def get_version():
     """ Find the version of the package"""
     version = None
-    version_file = os.path.join(BASEDIR, 'replace_package_name', 'version.py')
+    version_file = os.path.join(BASEDIR, 'ovos_intent_plugin_adapt', 'version.py')
     major, minor, build, alpha = (None, None, None, None)
     with open(version_file) as f:
         for line in f:
